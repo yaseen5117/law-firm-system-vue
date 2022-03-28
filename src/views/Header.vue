2274b60bd10ad1d27ce225fc5ef70477b081f16e
Hamza Aslam <imhamza@outlook.com>
2022-03-28 16:28:58 +0500

    test

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- ======= Header ======= -->
   <header id="header" class="fixed-top">
-    <PageLoader />
+    
     <div class="container d-flex align-items-center justify-content-between">
       <h1 class="logo"><router-link to="/">LFMS</router-link></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
