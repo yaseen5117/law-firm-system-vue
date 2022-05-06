ceed3d4fda0e6af195e7fc697d8dfbd8978d640c
Hamza Aslam <imhamza@outlook.com>
2022-05-06 18:43:36 +0500

    remove header by default

--- src/views/petition-index/PetitionIndexDetails.vue
+++ src/views/petition-index/PetitionIndexDetails.vue
@@ -406,7 +406,7 @@ export default {
       id: this.$route.params.id, //this is the id from the browser
       horizontalView: false, //it will show vertical images by default
       activePage: null,
-      removePageHeader: false,
+      removePageHeader: true,
       selected_attachment_ids: [],
       selectedAllToDelete: false,
       showDeleteBtn: false,
