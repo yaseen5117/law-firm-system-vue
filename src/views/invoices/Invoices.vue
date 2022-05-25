ff0694027f860a7fec1a6729e20118170ecc9d19
Hamza Aslam <imhamza@outlook.com>
2022-05-25 17:28:51 +0500

    updates

--- src/views/invoices/Invoices.vue
+++ src/views/invoices/Invoices.vue
@@ -241,7 +241,7 @@ export default {
         var dd = String(today.getDate()).padStart(2, '0');
         var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
         var yyyy = today.getFullYear();
-        today = mm + '/' + dd + '/' + yyyy;
+        today = dd + '/' + mm + '/' + yyyy;
         invoice.paid_date = today;
       }
       this.openModal(invoice)
