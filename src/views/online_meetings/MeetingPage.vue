de8568538eb4b2c1e961bf7295816d8c22f63b2e
Ghulam Yaseen <ghulamyaseenmalik206@gmail.com>
2022-10-17 15:54:47 +0500

    add loading layout on meet page

--- src/views/online_meetings/MeetingPage.vue
+++ src/views/online_meetings/MeetingPage.vue
@@ -1,32 +1,32 @@
 <template>
-  <main id="main">
-    <page-header
-      :title="userMeeting.meeting_id_public"
-      :copyToClipBtn="true"
-      :hideBreadCrumbs="true"
-      :route_object="route_obj"
-      :createMeetingBtn="createMeetingBtn"
-      :createMeetingBtnText="createMeetingBtnText"
-      @afterMeetingCreate="getUserMeeting"
-    />
-    <section id="services" class="services section-bg">
-      <div class="container" data-aos="fade-up">
-        <div class="row">
-          <div class="col-12 col-md-12">
-            <iframe
-              :src="
-                userMeeting.host_meeting_iframe +
-                '&displayName=ELawFirmPk&pipButton=off'
-              "
-              allow="camera; microphone; fullscreen; speaker; display-capture"
-              style="height: 700px; width: 100%"
-            ></iframe>
+  <BlockUI :blocked="isLoaded" :fullScreen="true">
+    <main id="main">
+      <page-header
+        :title="userMeeting.meeting_id_public"
+        :copyToClipBtn="true"
+        :hideBreadCrumbs="true"
+        :createMeetingBtn="createMeetingBtn"
+        :createMeetingBtnText="createMeetingBtnText"
+        @afterMeetingCreate="getUserMeeting"
+      />
+      <section id="services" class="services section-bg">
+        <div class="container" data-aos="fade-up">
+          <div class="row">
+            <div class="col-12 col-md-12">
+              <iframe
+                :src="
+                  userMeeting.host_meeting_iframe +
+                  '&displayName=ELawFirmPk&pipButton=off'
+                "
+                allow="camera; microphone; fullscreen; speaker; display-capture"
+                style="height: 700px; width: 100%"
+              ></iframe>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  </main>
-
+      </section>
+    </main>
+  </BlockUI>
   <!-- End #main -->
 </template>
 
@@ -41,9 +41,7 @@ export default {
   components: { PageHeader },
   data() {
     return {
-      route_obj: {
-        name: "create-petition",
-      },
+      isLoaded: false,
       createMeetingBtn: true,
       createMeetingBtnText: "New Meeting",
       base_url: process.env.VUE_APP_SERVICE_URL,
@@ -61,6 +59,7 @@ export default {
   },
   methods: {
     getUserMeeting() {
+      this.isLoaded = true;
       if (true) {
         var headers = {
           Authorization: `Bearer ` + localStorage.getItem("lfms_user"),
@@ -69,9 +68,11 @@ export default {
         axios
           .post(url, {}, { headers })
           .then((response) => {
+            this.isLoaded = false;
             this.userMeeting = response.data.meeting;
           })
           .catch((error) => {
+            this.isLoaded = false;
             this.$notify({
               type: "error",
               title: "Something went wrong!",
